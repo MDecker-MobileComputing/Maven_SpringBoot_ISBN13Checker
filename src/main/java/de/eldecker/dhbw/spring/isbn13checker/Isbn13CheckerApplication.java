801b0a3d87fd2da9cc2faf436627fd5b29bbdd6f
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-05-25 13:24:12 +0200

    Zustand unmittelbar nach Erzeugung mit Spring Initializer https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.5.0&packaging=jar&jvmVersion=21&groupId=de.eldecker.dhbw.spring&artifactId=isbn13checker&name=ISBN13-Checker&description=Webapp%20zum%20Checker%20von%20ISBN13-Pr%C3%BCfziffern&packageName=de.eldecker.dhbw.spring.isbn13checker&dependencies=thymeleaf

---
+++ src/main/java/de/eldecker/dhbw/spring/isbn13checker/Isbn13CheckerApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.isbn13checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isbn13CheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isbn13CheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eldecker/dhbw/spring/isbn13checker/Isbn13CheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.isbn13checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Isbn13CheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
